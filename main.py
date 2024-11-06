64fbc4dcc3b67574a3d601ab83160441f694d682
FelipePinheiro964 <fel.felipinheiro@gmail.com>
2024-11-06 22:23:13 +0000

    Adição de menu basico e inicio da questão 1

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 ##
 # inicio de sistema
 ##
+entrada = int(input("selecione o numero:"))#abrir dados em leitura
 
 
 f = open("steam_games.csv", "r") #abrir dados em leitura
 
+linhas = line.strip().split(',') # variavel linhas para facilitar leitura
 
-for line in f: # ler suas linhas
-  linhas = line.strip().split(',') # variavel linhas para facilitar leitura
-  #corrigido leitura defeituosa, mudança de ";" para ","
-  print(linhas) # imprimir todas as linhas, sem formatação, mas pode causar travamento
+if entrada == 1:
+  for line in f: # ler suas linhas
+    linhas = line.strip().split(',')
+    #corrigido leitura defeituosa, mudança de ";" para ","
+    print(linhas)  # imprimir todas as linhas, sem formatação, mas pode causar travamento
 
 
-f.close() # fecha o arquivo
+## Percentual de jogos pagos e gratuitos
+
+gratis = 0
+
+i = 0
+
+if entrada == 2:
+    for linha in f:
+      if i < 10:
+        if linhas[6] == "0":
+          gratis += 1
+          #print(linhas)
+
+
+print(gratis)
+
+f.close()# fecha o arquivo
