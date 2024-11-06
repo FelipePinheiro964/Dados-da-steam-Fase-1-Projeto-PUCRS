2fd28d16186fd8df8633972054cea94e579a417f
FelipePinheiro964 <fel.felipinheiro@gmail.com>
2024-11-06 23:49:31 +0000

    Primeira questão respondida, porcentagem de jogos gratuitos e pagos

--- main.py
+++ main.py
@@ -17,18 +17,27 @@ if entrada == 1:
 
 ## Percentual de jogos pagos e gratuitos
 
-gratis = 0
-
-i = 0
-
-if entrada == 2:
-    for linha in f:
-      if i < 10:
-        if linhas[6] == "0":
-          gratis += 1
-          #print(linhas)
+gratuito = 0
+pago = 0
+total_linhas = 0
 
 
-print(gratis)
+for line in f: # ler suas linhas
+  preco = line.strip().split(',')[7]
+  total_linhas = total_linhas + 1
+  #print(preco)
+  if preco == '0.0':
+    gratuito = gratuito + 1
+  else:
+    pago = pago + 1
+
+#print(gratuito, pago)
+#print(total_linhas)
+
+porcentagem_jogosgratuitos = (gratuito/total_linhas)*100
+porcentagem_jogospagos = (pago/total_linhas)*100
+
+print(f'% jogos gratuitos {porcentagem_jogosgratuitos:.2f}')
+print(f'% jogos pagos {porcentagem_jogospagos:.2f}')
 
 f.close()# fecha o arquivo
