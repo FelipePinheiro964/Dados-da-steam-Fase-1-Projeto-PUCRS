7ab781b16aaef92b236e0fd38a925b8111c7a279
FelipePinheiro964 <fel.felipinheiro@gmail.com>
2024-11-06 21:58:55 +0000

    Correção de erro em leitura

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ f = open("steam_games.csv", "r") #abrir dados em leitura
 
 
 for line in f: # ler suas linhas
-  linhas = line.strip().split(';') # variavel linhas para facilitar leitura
+  linhas = line.strip().split(',') # variavel linhas para facilitar leitura
+  #corrigido leitura defeituosa, mudança de ";" para ","
   print(linhas) # imprimir todas as linhas, sem formatação, mas pode causar travamento
 
 
