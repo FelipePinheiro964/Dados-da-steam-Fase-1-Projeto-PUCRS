08d5fa862c2e4fbc8a4fcf13ac14152265239356
FelipePinheiro964 <fel.felipinheiro@gmail.com>
2024-11-07 11:43:26 +0000

    Mudança no menu

--- main.py
+++ main.py
@@ -3,16 +3,21 @@
 ##
 entrada = int(input("selecione o numero:"))#abrir dados em leitura
 
+if entrada == 0: 
+  try: #validação dos dados
+    f = open("steam_games.csv", "r") #abrir dados em leitura
+    print("dados carregados")
+  except FileNotFoundError:
+    print("Erro ao carregar dados.")
 
-f = open("steam_games.csv", "r") #abrir dados em leitura
-
-linhas = line.strip().split(',') # variavel linhas para facilitar leitura
+ # variavel linhas para facilitar leitura
 
 if entrada == 1:
   for line in f: # ler suas linhas
+
     linhas = line.strip().split(',')
-    #corrigido leitura defeituosa, mudança de ";" para ","
-    print(linhas)  # imprimir todas as linhas, sem formatação, mas pode causar travamento
+
+    #print(linhas)  # imprimir todas as linhas, sem formatação, mas pode causar travamento
 
 
 ## Percentual de jogos pagos e gratuitos
@@ -21,23 +26,23 @@ gratuito = 0
 pago = 0
 total_linhas = 0
 
-
-for line in f: # ler suas linhas
-  preco = line.strip().split(',')[7]
-  total_linhas = total_linhas + 1
-  #print(preco)
-  if preco == '0.0':
-    gratuito = gratuito + 1
-  else:
-    pago = pago + 1
+if entrada == 2:
+  for line in f: # ler suas linhas
+    preco = line.strip().split(',')[7]
+    total_linhas = total_linhas + 1
+    #print(preco)
+    if preco == '0.0':
+      gratuito = gratuito + 1
+    else:
+      pago = pago + 1
 
 #print(gratuito, pago)
 #print(total_linhas)
 
-porcentagem_jogosgratuitos = (gratuito/total_linhas)*100
-porcentagem_jogospagos = (pago/total_linhas)*100
+    porcentagem_jogosgratuitos = (gratuito/total_linhas)*100
+    porcentagem_jogospagos = (pago/total_linhas)*100
 
-print(f'% jogos gratuitos {porcentagem_jogosgratuitos:.2f}')
-print(f'% jogos pagos {porcentagem_jogospagos:.2f}')
+    print(f'% jogos gratuitos {porcentagem_jogosgratuitos:.2f}')
+    print(f'% jogos pagos {porcentagem_jogospagos:.2f}')
 
 f.close()# fecha o arquivo
