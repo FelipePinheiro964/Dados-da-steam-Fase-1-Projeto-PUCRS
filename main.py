c30c93b4ff4ee920df9688a56499732a8edc8251
FelipePinheiro964 <fel.felipinheiro@gmail.com>
2024-11-07 13:55:35 +0000

    Remoção de "ifs" e adição de "cases", melhor desempenho no programa, terceira questão não resolvida

--- main.py
+++ main.py
@@ -2,13 +2,16 @@
 # inicio de sistema
 ##
 
+
 try: #validação dos dados
    f = open("steam_games.csv", "r") #abrir dados em leitura
    print("dados carregados")
+
 except FileNotFoundError: # Notificar erro
     print("Erro ao carregar dados.")
     print("Programa finalizado, tente novamente.")
     raise SystemExit
+
 
 # Iniciar loop do sistema
 while True: 
@@ -30,36 +33,55 @@ while True:
 ##
 
  # Imprimir todo o arquivo
+  match entrada:
 
-  if entrada == 1:
-    for line in f: # ler suas linhas
-      linhas = line.strip().split(',')
-      #print(linhas)  # imprimir todas as linhas, sem formatação, mas pode causar travamento
+    case 1:
+      for line in f: # ler suas linhas
+        linhas = line.strip().split(',')
+        #print(linhas)  # imprimir todas as linhas, sem formatação, mas pode causar travamento
 
 
 ## Percentual de jogos pagos e gratuitos
 
-  gratuito = 0
-  pago = 0
-  total_linhas = 0
+    case 2:
+      gratuito = 0
+      pago = 0
+      total_linhas = 0
 
-  if entrada == 2:
-    for line in f: # ler suas linhas
-      preco = line.strip().split(',')[7]
-      total_linhas = total_linhas + 1
+      for line in f: # ler suas linhas
+        preco = line.strip().split(',')[7]
+        total_linhas = total_linhas + 1
     #print(preco)
-      if preco == '0.0':
-        gratuito = gratuito + 1
-      else:
-        pago = pago + 1
+        if preco == '0.0':
+          gratuito = gratuito + 1
+        else:
+          pago = pago + 1
 
-#print(gratuito, pago)
-#print(total_linhas)
+    #print(gratuito, pago)
+    #print(total_linhas)
 
-    porcentagem_jogosgratuitos = (gratuito/total_linhas)*100
-    porcentagem_jogospagos = (pago/total_linhas)*100
+      porcentagem_jogosgratuitos = (gratuito/total_linhas)*100
+      porcentagem_jogospagos = (pago/total_linhas)*100
 
-    print(f'% jogos gratuitos {porcentagem_jogosgratuitos:.2f}')
-    print(f'% jogos pagos {porcentagem_jogospagos:.2f}')
+      print(f'% jogos gratuitos {porcentagem_jogosgratuitos:.2f}')
+      print(f'% jogos pagos {porcentagem_jogospagos:.2f}')
 
-  f.close()# fecha o arquivo
+
+
+##
+#    Questão 3: jogo com melhor avaliação do usuario
+##
+
+
+    case 4:
+      maior_avaliacao = 0
+      nome_maior_avaliacao = 0
+      next(f)
+      for line in f: # ler suas linhas
+        avaliacao = line.strip().split(',')[19]
+        nome = line.strip().split(',')[1]
+        if float(avaliacao) > maior_avaliacao:
+            maior_avaliacao = avaliacao
+            nome_maior_avaliacao = nome
+        print(avaliacao)
+
