126bfe598af861aa90bac8b4d500e9cb4efa55cf
FelipePinheiro964 <fel.felipinheiro@gmail.com>
2024-11-07 12:16:06 +0000

    Validação básica de erro e aplicação do loop para menu

--- main.py
+++ main.py
@@ -1,40 +1,44 @@
 ##
 # inicio de sistema
 ##
-entrada = int(input("selecione o numero:"))#abrir dados em leitura
 
-if entrada == 0: 
+# Iniciar loop do sistema
+while True: 
+  entrada = int(input("selecione o numero:"))#Selecionar questão/função
+
+##
+# Operações do sistema
+##
+  
   try: #validação dos dados
-    f = open("steam_games.csv", "r") #abrir dados em leitura
-    print("dados carregados")
-  except FileNotFoundError:
-    print("Erro ao carregar dados.")
+      f = open("steam_games.csv", "r") #abrir dados em leitura
+      print("dados carregados")
+  except FileNotFoundError: # Notificar erro
+      print("Erro ao carregar dados.")
 
- # variavel linhas para facilitar leitura
+ # Imprimir todo o arquivo
 
-if entrada == 1:
-  for line in f: # ler suas linhas
-
-    linhas = line.strip().split(',')
-
-    #print(linhas)  # imprimir todas as linhas, sem formatação, mas pode causar travamento
+  if entrada == 1:
+    for line in f: # ler suas linhas
+      linhas = line.strip().split(',')
+      #print(linhas)  # imprimir todas as linhas, sem formatação, mas pode causar travamento
 
 
 ## Percentual de jogos pagos e gratuitos
 
-gratuito = 0
-pago = 0
-total_linhas = 0
+  gratuito = 0
+  pago = 0
+  total_linhas = 0
 
-if entrada == 2:
-  for line in f: # ler suas linhas
-    preco = line.strip().split(',')[7]
-    total_linhas = total_linhas + 1
+  if entrada == 2:
+    for line in f: # ler suas linhas
+      preco = line.strip().split(',')[7]
+      total_linhas = total_linhas + 1
     #print(preco)
-    if preco == '0.0':
-      gratuito = gratuito + 1
-    else:
-      pago = pago + 1
+      if preco == '0.0':
+        gratuito = gratuito + 1
+      else:
+        pago = pago + 1
 
 #print(gratuito, pago)
 #print(total_linhas)
@@ -45,4 +49,4 @@ if entrada == 2:
     print(f'% jogos gratuitos {porcentagem_jogosgratuitos:.2f}')
     print(f'% jogos pagos {porcentagem_jogospagos:.2f}')
 
-f.close()# fecha o arquivo
+  f.close()# fecha o arquivo
